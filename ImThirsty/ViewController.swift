a7efb5cf780c3c1576fe5185f4bbf21c44e583da
Vasilis Sarris <vas82@pitt.edu>
2019-09-18 00:10:04 +0200

    Initial Commit

---
+++ ImThirsty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImThirsty
+//
+//  Created by Tristan Charpentier on 18/09/2019.
+//  Copyright © 2019 Tristan Charpentier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
