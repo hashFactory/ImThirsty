55f1092f473b26080bac03e0e5cd9a9507fb2094
Vasilis Sarris <vas82@pitt.edu>
2019-09-22 09:49:07 +0200

    First push, working

---
+++ ImThirsty/DataInput.swift
@@ -0,0 +1,127 @@
+//
+//  DataInput.swift
+//  ImThirsty
+//
+//  Created by Tristan Charpentier on 20/09/2019.
+//  Copyright © 2019 Tristan Charpentier. All rights reserved.
+//
+
+import Foundation
+
+class DataInput {
+    
+    typealias DataPoint = (lat: Double, long: Double, dist: Double, heading: Double)
+    
+    var myDoubles = Array<Double>()
+    var allCoordinates:[(lat: Double, long: Double)] = []
+    
+    func readFile(name: String) {
+        
+        if let path = Bundle.main.path(forResource: name, ofType: "txt") {
+            do {
+                let fileData = try String(contentsOfFile: path, encoding: .utf8).replacingOccurrences(of: " ", with: "", options: String.CompareOptions.literal, range: nil)
+                myDoubles = fileData.components(separatedBy: .newlines).compactMap(Double.init)
+                print(myDoubles[1387])
+            } catch {
+                print(error)
+            }
+            
+            process()
+        }
+    }
+    
+    func process() {
+        
+        for i in 0..<(myDoubles.count / 2) {
+            allCoordinates.append((myDoubles[i * 2], myDoubles[i * 2 + 1]))
+        }
+        
+    }
+    
+    func toRadians(value: Double) -> Double {
+        return value * .pi / 180.0
+    }
+    
+    func hyp(a: Double, b: Double) -> Double {
+        return sqrt(a * a + b * b)
+    }
+    
+    func getHeading(lat1: Double, long1: Double, lat2: Double, long2: Double, currentHeading: Double) -> Double {
+        
+        let startLat = toRadians(value: lat2)
+        let startLong = toRadians(value: long2)
+        let finalLat = toRadians(value: lat1)
+        let finalLong = toRadians(value: long1)
+        
+        let y = sin(finalLong - startLong) * cos(finalLat)
+        let x = cos(startLat) * sin(finalLat) - sin(startLat) * cos(finalLat) * cos(finalLong - startLong)
+        let bearing = (atan2(y, x) * (180.0 / .pi) + 360.0 - currentHeading).truncatingRemainder(dividingBy: 360.0)
+        
+        return bearing
+    }
+
+    func getRealDistance(lat1: Double, long1: Double, lat2: Double, long2: Double) -> Double {
+        
+        // Haversine formula
+        let earthRadius = 6371000.0 // Meters
+        let phi1 = toRadians(value: lat1)
+        let phi2 = toRadians(value: long1)
+        
+        let deltaPhi = toRadians(value: lat2 - lat1)
+        let deltaLambda = toRadians(value: long2 - long1)
+        
+        let a = sin(deltaPhi / 2.0) * sin(deltaPhi / 2.0) + cos(phi1) * cos(phi2) * sin(deltaLambda / 2.0) * sin(deltaLambda / 2.0)
+        let c = 2.0 * atan2(sqrt(a), sqrt(1.0 - a))
+        
+        return earthRadius * c
+    }
+    
+    func getNewMax(topNums: [DataPoint]) -> (Int, Double) {
+        
+        print(topNums)
+        var maxValue: (index: Int, dist: Double) = (0, 0)
+        
+        for i in 0..<topNums.count {
+            
+            if topNums[i].dist > maxValue.dist {
+                maxValue = (i, topNums[i].dist)
+            }
+            
+        }
+        
+        return maxValue
+    }
+    
+    // Finds the top #num quickly for further processing later on
+    func quickFind(lat: Double, long: Double, heading: Double, num: Int) -> [DataPoint] {
+        
+        var topNums: [DataPoint] = []
+        var maxValue: (index: Int, dist: Double) = (0, Double.infinity)
+        
+        for _ in 1...num {
+            topNums.append((0, 0, Double.infinity, 0))
+        }
+        
+        allCoordinates.forEach { coords in
+            
+            let distance = hyp(a: coords.lat - lat, b: coords.long - long)
+            
+            if distance < maxValue.dist {
+                topNums[maxValue.index] = (coords.lat, coords.long, distance, 0)
+                
+                maxValue = getNewMax(topNums: topNums)
+            }
+        }
+        
+        for i in 0..<num {
+            topNums[i].dist = getRealDistance(lat1: topNums[i].lat, long1: topNums[i].long, lat2: lat, long2: long)
+            topNums[i].heading = getHeading(lat1: topNums[i].lat, long1: topNums[i].long, lat2: lat, long2: long, currentHeading: heading)
+        }
+        
+        topNums.sort(by: {$0.dist < $1.dist})
+        
+        print(topNums)
+        
+        return topNums
+    }
+}
--- ImThirsty/ViewController.swift
+++ ImThirsty/ViewController.swift
@@ -4,17 +4,145 @@
 //
 //  Created by Tristan Charpentier on 18/09/2019.
 //  Copyright © 2019 Tristan Charpentier. All rights reserved.
-//
+
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let locationManager = CLLocationManager()
+    var timer = Timer()
+    var data = DataInput();
+    
+    var results = [DataInput.DataPoint]()
+    var previousHeadings = [Double]()
+    
+    /*private func loadResults() {
+        
+        // guard let result1 =
+    }*/
+    
+    @IBOutlet weak var myLocationDisplay: UITextView!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.refreshButton.addTarget(self, action: Selector(("refreshLocation")), for: .touchUpInside)
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        data.readFile(name: "water2");
+        
+        if CLLocationManager.locationServicesEnabled() {
+            // locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+            locationManager.startUpdatingHeading()
+            
+            scheduledTimerWithTimeInterval()
+            // self.view.addSubview(label)
+            
+            refreshLocation()
+        }
+        
+        myLocationDisplay.textContainer.maximumNumberOfLines = 10
+        myLocationDisplay.textContainer.lineBreakMode = .byTruncatingTail
     }
+    
+    func scheduledTimerWithTimeInterval(){
+        // Scheduling timer to Call the function "updateCounting" with the interval of 1 seconds
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: Selector(("refreshLocation")), userInfo: nil, repeats: true)
+    }
+    
+    @objc func refreshLocation() {
 
+        guard let locationLatLong = locationManager.location else {
+            return
+        }
+        
+        guard let locationHeading = locationManager.heading else {
+            return
+        }
+        
+        let locationString = "Latitude:\t\t\(locationLatLong.coordinate.latitude)°N\nLongitude:\t\(locationLatLong.coordinate.longitude)°E\nBearing:\t\t\(locationHeading.magneticHeading)°"
+        
+        myLocationDisplay.text = locationString
+        
+        results = data.quickFind(lat: locationLatLong.coordinate.latitude, long: locationLatLong.coordinate.longitude, heading: locationHeading.magneticHeading, num: 10)
+        
+        self.tableView.reloadData()
+        
+        print("Horizontal: \(locationLatLong.horizontalAccuracy)\nVertical: \(locationLatLong.verticalAccuracy)")
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return results.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "ResultTableViewCell"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? ResultTableViewCell else {
+            fatalError("Couldn't instantiate cell")
+        }
+        
+        let result = results[indexPath.row]
+        var savedPrevious: Double = 0.0
+        
+        if previousHeadings.count < results.count {
+            self.previousHeadings.append(self.results[indexPath.row].heading)
+        }
+        else {
+            savedPrevious = self.previousHeadings[indexPath.row]
+            self.previousHeadings[indexPath.row] = self.results[indexPath.row].heading
+        }
+        
+        cell.distanceLabel.text = String(round(result.dist)) + " m"
+        
+        // TODO: ************ Implement image generation
+        cell.distanceDirection.transform = .identity
+        cell.distanceDirection.image = UIImage(named: "arrow")
+        cell.distanceDirection.transform = .identity
+        cell.distanceDirection.transform = CGAffineTransform(rotationAngle: CGFloat(data.toRadians(value: savedPrevious)))
+        UIView.animate(withDuration: 0.1) {
+            cell.distanceDirection.transform = CGAffineTransform(rotationAngle: CGFloat(self.data.toRadians(value:  self.results[indexPath.row].heading)))
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        print(String("comgooglemaps://?center=" + String(self.results[indexPath.row].lat) + "," + String(self.results[indexPath.row].long) + "&zoom=14"))
+        let urlString = String("http://maps.google.com/?daddr=" + String(self.results[indexPath.row].lat) + "," + String(self.results[indexPath.row].long) + "&directionsmode=driving")
+        
+        if let url = URL(string: urlString)
+        {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            /*
+            if (UIApplication.shared.canOpenURL(URL(string:"comgooglemaps://")!)) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            } else {
+                print("Can't use comgooglemaps://");
+            }*/
+        }
+    }
+    
+    
 
 }
 
